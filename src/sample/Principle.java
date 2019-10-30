7eb1d7d3b2a45a5cb66d8692f00fb98934ff0d51
Amit Goldshtein <Amit.Goldshtein@s.afeka.ac.il>
2019-10-30 09:44:49 +0200

    Added inheritance to User class.
    Created constructor

--- src/sample/Principle.java
+++ src/sample/Principle.java
@@ -1,4 +1,13 @@
 package sample;
 
-public class Principle {
+public class Principle extends User {
+    private College college;
+
+    public Principle(String name, String email, College college) {
+        this.setName(name);
+        this.setEmail(email);
+        this.college = college;
+    }
+
+
 }
